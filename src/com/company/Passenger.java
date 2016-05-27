98610c21f20c6c8f13726619d863eec7f24e41a1
Raphael <gemini88mill@gmail.com>
2016-05-27 13:14:01 -0400

    working on passenger class

--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gemin on 5/11/2016.
  *
@@ -10,6 +13,9 @@ public class Passenger {
     private String passengerLuggageCount;
     private String passengerMealOption;
     private boolean isFirstClass;
+    private boolean isBusinessClass;
+    private boolean isWindowSeat;
+    private boolean isAisle;
 
     private String passengerName;
     private String passengerSeatRequest; //aisle window or neither
@@ -37,19 +43,82 @@ public class Passenger {
     public void assignPassengerToSeat(Passenger passenger, Seat[] seat, Airplane airplane){
         //gets passenger information and assigns passenger to the seats available
 
+        classLabeler(passenger);
+
         //check if seat is occupied
         Seat[] availableSeats = new Seat[airplane.getAirplaneMaxSeats()];
 
         for (int i = 0; i < seat.length; i++){
-            if(!seat[i].isOccupied()){
 
+            //if seat is not occupied, put seats in available seats
+            if(!seat[i].isOccupied()){
+                availableSeats[i] = seat[i];
             }
+
         }
 
+        //Seat[] classSpecificSeats = new Seat[availableSeats.length];
+        List<Seat> classSeats = new ArrayList<>();
+
+        for(int i = 0; i < availableSeats.length; i++){
+            if(availableSeats[i].isFirstClass() == passenger.isFirstClass()){
+                classSeats.add(availableSeats[i]);
+            }
+            if(availableSeats[i].isBusinessClass() == passenger.isBusinessClass()){
+                classSeats.add(availableSeats[i]);
+            }
+        }
+        //ex: Passenger(Joe smith, window, first, null)
+        //ex: Seat(1A, First Class, Window)
+
         //check passenger credentials
+
         //show seats available
         //mark seat for passenger
         //mark seat as occupied.
+    }
+
+    private void classLabeler(Passenger passenger) {
+        switch(passenger.getPassengerClassPreference()){
+            case "FIRST":
+                setFirstClass(true);
+                setBusinessClass(false);
+                break;
+            case "BUSINESS":
+                setFirstClass(false);
+                setBusinessClass(true);
+                break;
+            case "ECONOMY":
+                setFirstClass(false);
+                setBusinessClass(false);
+                break;
+            default:
+                System.err.println("Improper Selection, please check passenger credentials");
+                break;
+        }
+
+        switch (passenger.getPassengerSeatRequest()){
+            case "WINDOW":
+                setAisle(false);
+                setWindowSeat(true);
+                break;
+            case "AISLE":
+                setAisle(true);
+                setWindowSeat(false);
+                break;
+            case "NA":
+                setAisle(false);
+                setWindowSeat(false);
+                break;
+            default:
+                System.err.println("Improper Selection, please check passenger credentials");
+                break;
+        }
+    }
+
+    private Seat checkSeatSelection(String passengerClassPreference, Seat[] availableSeats, Passenger passenger) {
+
+        return null;
     }
 
 
@@ -109,5 +178,29 @@ public class Passenger {
 
     public void setPassengerSeatAssignment(String passengerSeatAssignment) {
         this.passengerSeatAssignment = passengerSeatAssignment;
+    }
+
+    public boolean isBusinessClass() {
+        return isBusinessClass;
+    }
+
+    public void setBusinessClass(boolean businessClass) {
+        isBusinessClass = businessClass;
+    }
+
+    public boolean isWindowSeat() {
+        return isWindowSeat;
+    }
+
+    public void setWindowSeat(boolean windowSeat) {
+        isWindowSeat = windowSeat;
+    }
+
+    public boolean isAisle() {
+        return isAisle;
+    }
+
+    public void setAisle(boolean aisle) {
+        isAisle = aisle;
     }
 }
